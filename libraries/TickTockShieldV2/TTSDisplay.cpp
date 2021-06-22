f531659051076ed056ad2c11a24b45e835e71cd1
Dmitry Naumov <dmitry.godsmack.naumov@gmail.com>
2021-06-22 14:02:04 +0300

    Updated lib according to latest drivers

--- libraries/TickTockShieldV2/TTSDisplay.cpp
+++ libraries/TickTockShieldV2/TTSDisplay.cpp
@@ -1,268 +1,231 @@
 /*
-  TTSDisplay.cpp
-
-  Author:Loovee
-  2013-11-21
- 
-  The MIT License (MIT)
-  Copyright (c) 2013 Seeed Technology Inc.
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
-#include <Arduino.h>
-#include <Streaming.h>
+ * TTSDisplay.cpp
+ * A library for the 4 digit display
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Frankie.Chu
+ * Create Time: 9 April,2012
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "TTSDisplay.h"
-
-//0~9,A,b,C,d,E,F,"-"," "
-const uchar TubeTab[] = 
+#include <Arduino.h>
+static int8_t TubeTab[] = {0x3f,0x06,0x5b,0x4f,
+                           0x66,0x6d,0x7d,0x07,
+                           0x7f,0x6f,0x77,0x7c,
+                           0x39,0x5e,0x79,0x71};//0~9,A,b,C,d,E,F
+TTSDisplay::TTSDisplay(uint8_t Clk, uint8_t Data)
 {
-    0x3f,0x06,0x5b,0x4f,
-    0x66,0x6d,0x7d,0x07,
-    0x7f,0x6f,0x77,0x7c,
-    0x39,0x5e,0x79,0x71,
-    0x40,0x00
-};
-    
-#define PINCLK      7                   // pin of clk 
-#define PINDTA      8                   // pin of data
-
-/*********************************************************************************************************
- * Function Name: TTSDisplay
- * Description:  init
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
+  Clkpin = Clk;
+  Datapin = Data;
+  pinMode(Clkpin,OUTPUT);
+  pinMode(Datapin,OUTPUT);
+}
 TTSDisplay::TTSDisplay()
 {
-    Clkpin  = PINCLK;
-    Datapin = PINDTA;
-    
-    pinMode(Clkpin,OUTPUT);
-    pinMode(Datapin,OUTPUT);
-    
-    for(int i=0; i<4; i++)
-    {
-        dtaDisplay[i] = 0x00;
-    }
-    
-    set();
-    //clear();
+  Clkpin = 7;
+  Datapin = 8;
+  pinMode(Clkpin,OUTPUT);
+  pinMode(Datapin,OUTPUT);
 }
-
-/*********************************************************************************************************
- * Function Name: display
- * Description:  display a num in certain location
- * Parameters: loca - location, 3-2-1-0
- *             num - number to display
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::display(uchar loca, uchar dta)
+void TTSDisplay::init(void)
 {
-
-    if(loca > 3 || loca < 0)return;
-
-    dtaDisplay[loca] = dta;
-
-    loca = 3-loca;
-
-    uchar segData = coding(dta);
-    
-    start();                            //start signal sent to TM1637 from MCU
-    writeByte(ADDR_FIXED);
-    stop();
-    
-    start();
-    writeByte(loca|0xc0);
-    writeByte(segData);
-    stop();
-    
-    start();
-    writeByte(Cmd_Dispdisplay);
-    stop();
-
+  clearDisplay();
 }
 
-/*********************************************************************************************************
- * Function Name: num
- * Description:  display a num  that 0 - 9999
- * Parameters: num - number to display
- * Return: None
-*********************************************************************************************************/
+int TTSDisplay::writeByte(int8_t wr_data)
+{
+  uint8_t i,count1;
+  for(i=0;i<8;i++)        //sent 8bit data
+  {
+    digitalWrite(Clkpin,LOW);
+    if(wr_data & 0x01)digitalWrite(Datapin,HIGH);//LSB first
+    else digitalWrite(Datapin,LOW);
+    wr_data >>= 1;
+    digitalWrite(Clkpin,HIGH);
+
+  }
+  digitalWrite(Clkpin,LOW); //wait for the ACK
+  digitalWrite(Datapin,HIGH);
+  digitalWrite(Clkpin,HIGH);
+  pinMode(Datapin,INPUT);
+
+  bitDelay();
+  uint8_t ack = digitalRead(Datapin);
+  if (ack == 0) 
+  {
+     pinMode(Datapin,OUTPUT);
+     digitalWrite(Datapin,LOW);
+  }
+  bitDelay();
+  pinMode(Datapin,OUTPUT);
+  bitDelay();
+  
+  return ack;
+}
+//send start signal to TTSDisplay
+void TTSDisplay::start(void)
+{
+  digitalWrite(Clkpin,HIGH);//send start signal to TTSDisplay
+  digitalWrite(Datapin,HIGH);
+  digitalWrite(Datapin,LOW);
+  digitalWrite(Clkpin,LOW);
+}
+//End of transmission
+void TTSDisplay::stop(void)
+{
+  digitalWrite(Clkpin,LOW);
+  digitalWrite(Datapin,LOW);
+  digitalWrite(Clkpin,HIGH);
+  digitalWrite(Datapin,HIGH);
+}
+//display function.Write to full-screen.
+void TTSDisplay::display(int8_t DispData[])
+{
+  int8_t SegData[4];
+  uint8_t i;
+  for(i = 0;i < 4;i ++)
+  {
+    SegData[i] = DispData[i];
+    InternalData[i] = DispData[i];
+  }
+  coding(SegData);
+  start();          //start signal sent to TTSDisplay from MCU
+  writeByte(ADDR_AUTO);//
+  stop();           //
+  start();          //
+  writeByte(Cmd_SetAddr);//
+  for(i=0;i < 4;i ++)
+  {
+    writeByte(SegData[i]);        //
+  }
+  stop();           //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
+//******************************************
+void TTSDisplay::display(uint8_t BitAddr,int8_t DispData)
+{
+    InternalData[BitAddr] = DispData;
+  int8_t SegData;
+  SegData = coding(DispData);
+  start();          //start signal sent to TTSDisplay from MCU
+  writeByte(ADDR_FIXED);//
+  stop();           //
+  start();          //
+  writeByte(BitAddr|0xc0);//
+  writeByte(SegData);//
+  stop();            //
+  start();          //
+  writeByte(Cmd_DispCtrl);//
+  stop();           //
+}
+
+void TTSDisplay::clearDisplay(void)
+{
+  display(0x00,0x7f);
+  display(0x01,0x7f);
+  display(0x02,0x7f);
+  display(0x03,0x7f);
+}
+//To take effect the next time it displays.
+void TTSDisplay::set(uint8_t brightness,uint8_t SetData,uint8_t SetAddr)
+{
+  Cmd_SetData = SetData;
+  Cmd_SetAddr = SetAddr;
+  Cmd_DispCtrl = 0x88 + brightness;//Set the brightness and it takes effect the next time it displays.
+}
+
+//Whether to light the clock point ":".
+//To take effect the next time it displays.
+void TTSDisplay::point(boolean PointFlag)
+{
+  _PointFlag = PointFlag;
+}
+void TTSDisplay::coding(int8_t DispData[])
+{
+  uint8_t PointData;
+  if(_PointFlag == POINT_ON)PointData = 0x80;
+  else PointData = 0;
+  for(uint8_t i = 0;i < 4;i ++)
+  {
+    if(DispData[i] == 0x7f)DispData[i] = 0x00;
+    else DispData[i] = TubeTab[DispData[i]] + PointData;
+  }
+}
+int8_t TTSDisplay::coding(int8_t DispData)
+{
+  uint8_t PointData;
+  if(_PointFlag == POINT_ON)PointData = 0x80;
+  else PointData = 0;
+  if(DispData == 0x7f) DispData = 0x00 + PointData;//The bit digital tube off
+  else DispData = TubeTab[DispData] + PointData;
+  return DispData;
+}
+void TTSDisplay::bitDelay(void)
+{
+	delayMicroseconds(50);
+}
+
 void TTSDisplay::num(int dta)
 {
     if(dta < 0 || dta > 9999) return;           // bad data
     
     //clear();
     
-    pointOff();
+    point(false);
     if(dta < 10)
     {
-        display(0, dta);
-        display(1, 0x7f);
+        display(3, dta);
         display(2, 0x7f);
-        display(3, 0x7f);
+        display(1, 0x7f);
+        display(0, 0x7f);
     }
     else if(dta < 100)
     {
-        display(1, dta/10);
-        display(0, dta%10);
-        display(2, 0x7f);
-        display(3, 0x7f);
+        display(0, dta/10);
+        display(1, dta%10);
+        display(1, 0x7f);
+        display(0, 0x7f);
     }
     else if(dta < 1000)
     {
-        display(2, dta/100);
-        display(1, (dta/10)%10);
-        display(0, dta%10);
-        display(3, 0x7f);
+        display(1, dta/100);
+        display(2, (dta/10)%10);
+        display(3, dta%10);
+        display(0, 0x7f);
     }
     else
     {
-        display(3, dta/1000);
-        display(2, (dta/100)%10);
-        display(1, (dta/10)%10);
-        display(0, dta%10);
+        display(0, dta/1000);
+        display(1, (dta/100)%10);
+        display(2, (dta/10)%10);
+        display(3, dta%10);
     }
 }
 
-/*********************************************************************************************************
- * Function Name: time
- * Description:  display time
- * Parameters: hour - hour
- *             min - minutes
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::time(uchar hour, uchar min)
-{
-    if(hour > 24 || hour < 0) return;           // bad data
-    if(min > 60 || min < 0) return;             // bad data
-
-    display(3, hour/10);
-    display(2, hour%10);
-    display(1, min/10);
-    display(0, min%10);
-}
-    
-/*********************************************************************************************************
- * Function Name: clear
- * Description:  clear all 
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::clear()
-{
-    display(0x00,0x7f);
-    display(0x01,0x7f);
-    display(0x02,0x7f);
-    display(0x03,0x7f);
-}
-
-/*********************************************************************************************************
- * Function Name: writeByte
- * Description:  write a byte to tm1636
- * Parameters: wr_data: data to write
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::writeByte(uchar wr_data)
-{
-    uchar i,count1;
-    for(i=0;i<8;i++)                                        // sent 8bit data
-    {
-        digitalWrite(Clkpin,LOW);
-        if(wr_data & 0x01)digitalWrite(Datapin,HIGH);       // LSB first
-        else digitalWrite(Datapin,LOW);
-        wr_data >>= 1;
-        digitalWrite(Clkpin,HIGH);
-    }
-    
-    digitalWrite(Clkpin,LOW);                               // wait for the ACK
-    digitalWrite(Datapin,HIGH);
-    digitalWrite(Clkpin,HIGH);
-    pinMode(Datapin,INPUT);
-    
-    while(digitalRead(Datapin))
-    {
-        count1 += 1;
-        if(200 == count1)
-        {
-            pinMode(Datapin,OUTPUT);
-            digitalWrite(Datapin,LOW);
-            count1 =0;
-        }
-        pinMode(Datapin,INPUT);
-    }
-    pinMode(Datapin,OUTPUT);
-
-}
-
-/*********************************************************************************************************
- * Function Name: start
- * Description:  send start signal to TTSDisplay
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::start(void)
-{
-    digitalWrite(Clkpin,HIGH);                              //send start signal to TM1637
-    digitalWrite(Datapin,HIGH);
-    digitalWrite(Datapin,LOW);
-    digitalWrite(Clkpin,LOW);
-}
-
-/*********************************************************************************************************
- * Function Name: stop
- * Description:  send end signal
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::stop(void)
-{
-    digitalWrite(Clkpin,LOW);
-    digitalWrite(Datapin,LOW);
-    digitalWrite(Clkpin,HIGH);
-    digitalWrite(Datapin,HIGH);
-}
-
-/*********************************************************************************************************
- * Function Name: set
- * Description:  init
- * Parameters: brightness - brightness
- *             SetDta - data
- *             SetAddr - address
- * Return: None
-*********************************************************************************************************/
-void TTSDisplay::set(uchar brightness,uchar SetData,uchar SetAddr)
-{
-    _brightness = brightness;
-    Cmd_SetData = SetData;
-    Cmd_SetAddr = SetAddr;
-    Cmd_Dispdisplay = 0x88 + brightness;
-}
-
-/*********************************************************************************************************
- * Function Name: pointOn
- * Description:  display :
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
 void TTSDisplay::pointOn()
 {
     _PointFlag = 1;
@@ -274,15 +237,9 @@ void TTSDisplay::pointOn()
     // }
 
     //FOR NEW DISPLAY
-    display(2, dtaDisplay[2]);
-}
+    display(1, InternalData[1]);
+}   
 
-/*********************************************************************************************************
- * Function Name: pointOff
- * Description:  no :
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
 void TTSDisplay::pointOff()
 {
     _PointFlag = 0;
@@ -294,24 +251,16 @@ void TTSDisplay::pointOff()
     // }
 
     //FOR NEW DISPLAY
-    display(2, dtaDisplay[2]);
+    display(1, InternalData[1]);
 }
 
-/*********************************************************************************************************
- * Function Name: coding
- * Description:  coding
- * Parameters: None
- * Return: None
-*********************************************************************************************************/
-uchar TTSDisplay::coding(uchar DispData)
+void TTSDisplay::time(uint8_t hour, uint8_t min)
 {
+    if(hour > 24 || hour < 0) return;           // bad data
+    if(min > 60 || min < 0) return;             // bad data
 
-    uchar PointData = _PointFlag ? 0x80 : 0x00;
-    DispData = (0x7f == DispData) ? PointData : (TubeTab[DispData]+PointData);
-    return DispData;
+    display(0, hour/10);
+    display(1, hour%10);
+    display(2, min/10);
+    display(3, min%10);
 }
-    
-   
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
--- libraries/TickTockShieldV2/TTSDisplay.h
+++ libraries/TickTockShieldV2/TTSDisplay.h
@@ -1,98 +1,80 @@
 /*
-  TTSDisplay.h
+ * TTSDisplay.h
+ * A library for the 4 digit display
+ *
+ * Copyright (c) 2012 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : Frankie.Chu
+ * Create Time: 9 April,2012
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-  Author:Loovee
-  2013-11-21
- 
-  The MIT License (MIT)
-  Copyright (c) 2013 Seeed Technology Inc.
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-*/
-
-#ifndef __TTSDISPLAY_H__
-#define __TTSDISPLAY_H__
-
-#ifndef uchar
-#define uchar unsigned char 
-#endif
-
-// definitions for TM1636
+#ifndef TM1637_h
+#define TM1637_h
+#include <inttypes.h>
+#include <Arduino.h>
+//************definitions for TTSDisplay*********************
 #define ADDR_AUTO  0x40
 #define ADDR_FIXED 0x44
-#define STARTADDR  0xc0
 
-// definitions for the clock point of the digit tube
+#define STARTADDR  0xc0
+/**** definitions for the clock point of the digit tube *******/
 #define POINT_ON   1
 #define POINT_OFF  0
-
-// definitions for brightness
+/**************definitions for brightness***********************/
 #define  BRIGHT_DARKEST 0
 #define  BRIGHT_TYPICAL 2
 #define  BRIGHTEST      7
 
-//Special characters index of tube table
-#define INDEX_NEGATIVE_SIGH    16
-#define INDEX_BLANK            17
-
-
-class TTSDisplay{
-
-
-private:
-
-    uchar Cmd_SetData;
-    uchar Cmd_SetAddr;
-    uchar Cmd_Dispdisplay;
-    uchar _PointFlag;                                   //_PointFlag=1:the clock point on
-    uchar _brightness;
-    uchar Clkpin;
-    uchar Datapin;
-    uchar dtaDisplay[4];
-    
-private:
-
-    void writeByte(uchar wr_data);                      //write 8bit data to tm1637
-    void start(void);                                   //send start bits
-    void stop(void);                                    //send stop bits
-    void set(uchar = BRIGHT_TYPICAL, uchar = 0x40, uchar = 0xc0);       //To take effect the next time it displays. 
-    uchar coding(uchar DispData);
-
-
-//********************************************************************************************************** 
-//*************************** USER INTERFACE ***************************************************************
-//********************************************************************************************************** 
-public:
-
+class TTSDisplay
+{
+  public:
+    uint8_t Cmd_SetData;
+    uint8_t Cmd_SetAddr;
+    uint8_t Cmd_DispCtrl;
+    boolean _PointFlag;     //_PointFlag=1:the clock point on
+    TTSDisplay(uint8_t, uint8_t);
     TTSDisplay();
-
-    void display(uchar loca, uchar dta);                // display a single num(0-9)
-    void num(int dta);                                  // display a num (0-9999)    
-    void time(uchar hour, uchar min);                   // display time, such as: 11:26
-    void clear();                                       // clear display
-    
+    void init(void);        //To clear the display
+    int  writeByte(int8_t wr_data);//write 8bit data to TTSDisplay
+    void start(void);//send start bits
+    void stop(void); //send stop bits
+    void display(int8_t DispData[]);
+    void display(uint8_t BitAddr,int8_t DispData);
+    void clearDisplay(void);
+    void set(uint8_t = BRIGHT_TYPICAL,uint8_t = 0x40,uint8_t = 0xc0);//To take effect the next time it displays.
+    void point(boolean PointFlag);//whether to light the clock point ":".To take effect the next time it displays.
+    void coding(int8_t DispData[]);
+    int8_t coding(int8_t DispData);
+    void bitDelay(void);
+    void num(int dta);
+    void time(uint8_t hour, uint8_t min);
     void pointOn();                                     // display :
-    void pointOff();                                    // undisplay :
+    void pointOff();
 
+  private:
+    uint8_t Clkpin;
+    uint8_t Datapin;
+     uint8_t InternalData[4];
 };
-
 #endif
-/*********************************************************************************************************
-  END FILE
-*********************************************************************************************************/
